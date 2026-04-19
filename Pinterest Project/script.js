2be61f9e1a81f55aa323285723defda160282c27
PRACHIPATEL-26 <prachiipatel04@gmail.com>
2026-04-19 21:09:37 +0530

    add content section

---
+++ Pinterest Project/script.js
@@ -0,0 +1,32 @@
+const data = [
+  {
+    id: 1,
+    title: "Mountain View",
+    image: "https://picsum.photos/id/1018/300/400"
+  },
+  {
+    id: 2,
+    title: "Beach Sunset",
+    image: "https://picsum.photos/id/1015/300/500"
+  },
+  {
+    id: 3,
+    title: "City Lights",
+    image: "https://picsum.photos/id/1011/300/450"
+  },
+  {
+    id: 4,
+    title: "Forest Path",
+    image: "https://picsum.photos/id/1019/300/420"
+  },
+  {
+    id: 5,
+    title: "Desert Sand",
+    image: "https://picsum.photos/id/1002/300/480"
+  },
+  {
+    id: 6,
+    title: "Snow Mountains",
+    image: "https://picsum.photos/id/1003/300/460"
+  }
+];
